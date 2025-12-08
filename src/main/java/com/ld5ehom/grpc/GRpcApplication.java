bb53868e62cc911a4f12153da92e8a08a3335fed
Taewook Park <155722702+ld5ehom@users.noreply.github.com>
2025-12-07 23:22:36 -0800

    gRPC Setup

---
+++ src/main/java/com/ld5ehom/grpc/GRpcApplication.java
@@ -0,0 +1,13 @@
+package com.ld5ehom.grpc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GRpcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GRpcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ld5ehom/grpc/GRpcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ld5ehom.grpc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GRpcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
